5218d50f477cbf006b835a25c0038ea908ca3beb
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2021-09-19 23:48:24 -0700

    Create NumbersRiddle.java

---
+++ Chapter 1 - Java Coding Fundamentals/Activity 1.1.6/NumbersRiddle.java
@@ -0,0 +1,13 @@
+/*
+* Programmer: Miguel Villa Floran
+* Date: 9/19/2021
+* Course name: AP Computer Science A
+* Section: Activity 1.1.6 Numbers Riddle
+* Purpose/Assignment: NumbersRiddle 
+*/
+
+public class NumbersRiddle {
+  public static void main(String[] args) {
+
+  } // main
+} // NumbersRiddle class
