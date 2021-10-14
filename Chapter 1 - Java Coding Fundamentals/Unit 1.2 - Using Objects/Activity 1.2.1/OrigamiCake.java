c8c4548f7380df32950fce37e9527d8e9825c6bc
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2021-10-14 11:05:12 -0700

    Create OrigamiCake.java

---
+++ Chapter 1 - Java Coding Fundamentals/Unit 1.2 - Using Objects/Activity 1.2.1/OrigamiCake.java
@@ -0,0 +1,22 @@
+/*
+* Programmer: Miguel Villa Floran
+* Date: 10/14/2021
+* Course name: AP Computer Science A
+* Section: Activity 1.2.1 Objects: Instances of Classes
+* Purpose/Assignment: OrigamiCake 
+*/
+
+public class OrigamiCake {
+  // attributes
+  int size;
+  String color;
+
+  // behaviors
+  placeTwoSheetsTogether();
+  foldInHalfOnDiagonal();
+  foldTopDown();
+  foldBottomUp();
+  makeFanFold();
+  foldTwoSidesBack();
+  makeThreeLightFolds();
+} // OrigamiCake class
