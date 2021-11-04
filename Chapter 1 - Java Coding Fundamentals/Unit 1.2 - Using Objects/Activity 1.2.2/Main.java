7df13e3ca7a485f24abbde509fa7b1d717a77bb9
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2021-11-04 14:47:29 -0700

    Create Main.java

---
+++ Chapter 1 - Java Coding Fundamentals/Unit 1.2 - Using Objects/Activity 1.2.2/Main.java
@@ -0,0 +1,14 @@
+/*
+* Programmer: Miguel Villa Floran
+* Date: 10/14/2021
+* Course name: AP Computer Science A
+* Section: Activity 1.2.2 Creating and Storing Objects
+* Purpose/Assignment: Book 
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    Book bk1 = new Book(100);
+    Book bk1 = new Book(200);
+  } // main method
+} // Main class
